526b46e26a461ba7dc5bf263b5976a1fe99f37c0
Bo Bramer <bramer.bo@gmail.com>
2021-04-23 07:57:40 -0700

    Added the 3D engine

---
+++ src/com/bramerlabs/engine/objects/ObjectLoader.java
@@ -0,0 +1,15 @@
+package com.bramerlabs.engine.objects;
+
+public class ObjectLoader {
+
+    /**
+     * loads an object
+     * @param pathToFile - the path to the resource file
+     * @return - the render object
+     */
+    public static RenderObject loadObject(String pathToFile) {
+
+        return null;
+    }
+
+}
