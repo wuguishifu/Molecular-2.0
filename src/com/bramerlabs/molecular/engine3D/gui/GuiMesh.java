e52b96e861bbdb5fa1aeaa018bda56d135520b8e
Bo Bramer <bramer.bo@gmail.com>
2021-09-28 22:09:54 -0700

    Added default gui meshes, added gui mesh construction system

--- src/com/bramerlabs/molecular/engine3D/gui/GuiMesh.java
+++ src/com/bramerlabs/molecular/engine3D/gui/GuiMesh.java
@@ -1,9 +1,39 @@
 package com.bramerlabs.molecular.engine3D.gui;
 
-public class GuiMesh {
+import com.bramerlabs.molecular.engine3D.graphics.mesh.Mesh;
+import org.lwjgl.opengl.GL46;
+import org.lwjgl.system.MemoryUtil;
+
+import java.nio.FloatBuffer;
+
+public class GuiMesh extends Mesh {
+
+    public GuiMesh(GuiVertex[] vertices, int[] indices) {
+        super(vertices, indices);
+    }
 
     public boolean inBounds(float mouseX, float mouseY) {
         return false;
     }
 
+    @Override
+    public void create() {
+        vao = GL46.glGenVertexArrays();
+        GL46.glBindVertexArray(vao);
+        makePositionBuffer();
+        makeTextureCoordBuffer();
+        makeIndexBuffer();
+    }
+
+    public void makeTextureCoordBuffer() {
+        FloatBuffer textureBuffer = MemoryUtil.memAllocFloat(vertices.length * 2);
+        float[] textureData = new float[vertices.length * 2];
+        for (int i = 0; i < vertices.length; i++) {
+            GuiVertex vertex = (GuiVertex) vertices[i];
+            textureData[2 * i    ] = vertex.getTextureCoord().x;
+            textureData[2 * i + 1] = vertex.getTextureCoord().y;
+        }
+        textureBuffer.put(textureData).flip();
+        tbo = storeData(textureBuffer, TEXTURE_COORD, 2);
+    }
 }
---
+++ src/com/bramerlabs/molecular/engine3D/gui/def/ButtonMesh.java
@@ -0,0 +1,24 @@
+package com.bramerlabs.molecular.engine3D.gui.def;
+
+import com.bramerlabs.molecular.engine3D.gui.GuiMesh;
+import com.bramerlabs.molecular.engine3D.gui.GuiVertex;
+import com.bramerlabs.molecular.engine3D.math.vector.Vector2f;
+
+public class ButtonMesh extends GuiMesh {
+
+    public ButtonMesh(Vector2f position, Vector2f size) {
+        super(
+                new GuiVertex[]{
+                        new GuiVertex(position.x, position.y, 0, 0),
+                        new GuiVertex(position.x + size.x, position.y, 1, 0),
+                        new GuiVertex(position.x + size.x, position.y + size.y, 1, 1),
+                        new GuiVertex(position.x, position.y + size.y, 0, 1)
+                },
+                new int[]{
+                        0, 1, 2,
+                        0, 2, 3
+                }
+        );
+    }
+
+}
