c72f777b256b1479f480ca93b9f64bb47b98abd0
Bo Bramer <bramer.bo@gmail.com>
2021-09-30 20:49:37 -0700

    Created the Gui mesh renderer. Added model vectors and mtarix4f to the gui object class.

--- src/com/bramerlabs/molecular/engine3D/gui/GuiObject.java
+++ src/com/bramerlabs/molecular/engine3D/gui/GuiObject.java
@@ -1,15 +1,31 @@
 package com.bramerlabs.molecular.engine3D.gui;
 
+import com.bramerlabs.molecular.engine3D.math.matrix.Matrix4f;
+import com.bramerlabs.molecular.engine3D.math.vector.Vector2f;
+import com.bramerlabs.molecular.engine3D.math.vector.Vector3f;
+
 public class GuiObject {
 
     public GuiMesh mesh;
     public GuiSelectionBox selectionBox;
 
+    public Vector3f position;
+    public Vector3f rotation;
+    public Vector3f scale;
+
+    public Matrix4f model;
+
     private GuiObject.OnClickListener onClickListener;
 
-    public GuiObject(GuiMesh mesh, GuiSelectionBox selectionBox) {
+    public GuiObject(GuiMesh mesh, GuiSelectionBox selectionBox, Vector3f position, Vector3f rotation, Vector3f scale) {
         this.mesh = mesh;
         this.selectionBox = selectionBox;
+        this.position = position;
+        this.rotation = rotation;
+        this.scale = scale;
+
+        this.model = Matrix4f.transform(position, rotation, scale);
+
         this.onClickListener = buttonCode -> false;
     }
 
